ebd42defded4bad83bc19ee1992b8260b4b04287
bmwieczorek <bmwieczorek@gmail.com>
2025-12-16 20:26:58 +0100

    added fix for macbookair

--- pip3-install.sh
+++ pip3-install.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
-pip3 uninstall -y -r <(pip3 freeze | grep -v my-package)
+pip3 uninstall -y -r <(pip3 freeze)
 
 pip3 install --upgrade pip
 
 #pip3 install \
 # google-cloud-bigquery \
 # --constraint constraints.txt
+
+# mackbookair issue - needs Cython==0.29.37
+# pip3 install "cython<3.0.0" wheel
+# pip3 install "pyyaml==5.4.1" --no-build-isolation
+# pip3 install "numpy==1.24.2"
 
 pip3 install \
  -r requirements.txt \
